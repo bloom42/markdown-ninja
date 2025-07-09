f7f082da7c18441d6c65fdaff5ac6ee78510ad09
bloom <>
2025-07-09 10:12:02 +0000

    udpate pingoo-sdk

--- pingoo-go/wasm_host_functions.go
+++ pingoo-go/wasm_host_functions.go
@@ -2,20 +2,12 @@ package pingoo
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 	"strings"
 	"time"
 
 	"github.com/bloom42/stdx-go/retry"
 )
-
-type lookupHostInput struct {
-	IpAddress string `json:"ip_address"`
-}
-
-type lookupHostOutput struct {
-	Hostname string `json:"hostname"`
-}
 
 func (client *Client) resolveHostForIp(ctx context.Context, ip string) (string, error) {
 	logger := client.getLogger(ctx)
@@ -26,7 +18,9 @@ func (client *Client) resolveHostForIp(ctx context.Context, ip string) (string, 
 		return retryErr
 	}, retry.Context(ctx), retry.Attempts(5), retry.Delay(10*time.Millisecond))
 	if err != nil {
-		logger.Warn(fmt.Sprintf("pingoo: error resolving hostname for IP [%s]: %s", ip, err.Error()))
+		logger.Warn("pingoo: error resolving hostname for IP to verify bot",
+			slog.String("ip", ip), slog.String("error", err.Error()),
+		)
 		return "", nil
 	}
 
