6f8fcc6cb5321d68ae679723f0e31ba941ccdf41
bloom <>
2026-04-01 07:42:17 +0000

    server: update AddAltSvcHeader middleware

--- pkg/server/middlewares/server_header.go
+++ pkg/server/middlewares/server_header.go
@@ -1,11 +1,16 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/skerkour/stdx-go/httpx"
+)
 
 // SetServerHeader set's the Server HTTP header to "Markdown Ninja"
 func SetServerHeader(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Server", "Markdown Ninja")
+		w.Header().Add(httpx.HeaderAltSvc, `h3=":443"; ma=86400`)
 		next.ServeHTTP(w, req)
 	}
 
